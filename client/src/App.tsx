da975def3e4a893dc48169578b2c1f14132941d4
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-03-26 23:15:33 -0500

    Installed Dependencies needed for chart.js, made sample data to test on, and made data request from World Bank

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  return <div className='App'></div>
+  const [data, setData] = useState('SP.POP.TOTL')
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://data.nasdaq.com/api/v3/datatables/WB/DATA?series_id=${data}&country_code=CUB&api_key=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((res) => console.log(res.data))
+  }, [])
+
+  return <div className='App'>YO</div>
 }
 
 export default App
---
+++ client/src/exampleData.ts
@@ -0,0 +1,5 @@
+interface ExampleData {
+  [index: number]: string | number
+}
+
+export const exampleData: Array<ExampleData> = [['787J3ssd', 'label']]
