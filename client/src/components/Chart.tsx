90fdda60e1d6dbd5ed776c9334a2765071751c3b
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-04-07 23:24:00 -0500

    Carried on development

--- client/src/components/Chart.tsx
+++ client/src/components/Chart.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Bar } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto'
 
-export default function Chart({ data }) {
-  return <Bar data={data} />
+// export default function Chart({ data }) {
+//   return <Bar data={data} />
+// }
+
+export default function Chart() {
+  return <div>HIEy</div>
 }
